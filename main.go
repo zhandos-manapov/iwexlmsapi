88fa5d2d7e9cb22e27045d39a15b087eabc6c05d
“Adilgit config --global user.name “Adil <adilkalbaev2004@gmail.com>
2023-08-23 13:15:08 +0600

    add branch

--- main.go
+++ main.go
@@ -9,38 +9,43 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
 func loadEnv() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading environment variables file")
-	}
+ err := godotenv.Load(".env")
+ if err != nil {
+  log.Fatalf("Error loading environment variables file")
+ }
 }
 
 func main() {
 
-	loadEnv()
+ loadEnv()
 
-	database.ConnectToDB()
-	defer database.DisconnectFromDB()
+ database.ConnectToDB()
+ defer database.DisconnectFromDB()
 
-	validate := validator.New()
-	xvalidator.InitValidator(validate)
+ validate := validator.New()
+ xvalidator.InitValidator(validate)
 
-	app := fiber.New(fiber.Config{
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			code := fiber.StatusInternalServerError
+ app := fiber.New(fiber.Config{
+  ErrorHandler: func(c *fiber.Ctx, err error) error {
+   code := fiber.StatusInternalServerError
 
-			var e *fiber.Error
-			if errors.As(err, &e) {
-				code = e.Code
-			}
-			return c.Status(code).JSON(models.ServerError{Message: err.Error()})
-		},
-	})
+   var e *fiber.Error
+   if errors.As(err, &e) {
+    code = e.Code
+   }
+   return c.Status(code).JSON(models.ServerError{Message: err.Error()})
+  },
+ })
+ app.Use(cors.New(cors.Config{
+  AllowOrigins: "*",
+  AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+ }))
 
-	setupRoutes(app)
-	app.Listen(":3030")
+ setupRoutes(app)
+ app.Listen(":3030")
 }
