16382908dc73a3ba43e9ab60ff39efb3a250ce17
zhandos-manapov <zandosmanapov@gmail.com>
2023-08-28 10:52:17 +0600

    fix: spell mistake fix

--- models/regionModel.go
+++ models/regionModel.go
@@ -1,7 +1,7 @@
 package models
 
 type Region struct {
-	ID         int    `json:"id"`
-	RegionName string `json:"region_name"`
-	CountyID   int    `json:"county_id"`
+	ID         int    `json:"id" db:"id"`
+	RegionName string `json:"region_name" db:"region_name"`
+	CountyID   int    `json:"country_id" db:"country_id"`
 }
